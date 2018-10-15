09f58fa55b7771af5b86fc272cb4de800876eb32
Oscar Vallarino <oscarito_vallarino@hotmail.com>
2018-10-15 22:13:12 +0100

    Added 3 scenes
    Added:
    Main Menu
    Options
    Game
    Pause
    
    They are all link together

---
+++ Unity Project/The legacy of Kresnik/Assets/Scripts/GUI/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject mainMenu;
+
+    public void Options()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
